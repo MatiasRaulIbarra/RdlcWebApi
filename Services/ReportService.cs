b16690ce6d6a21b1bb3fdc0b19c10cf66be0378d
MatiasRaulIbarra <matiasibarra96@hotmail.com>
2022-10-24 11:58:10 -0300

    subiendo proyecto

---
+++ Services/ReportService.cs
@@ -0,0 +1,65 @@
+﻿using AspNetCore.Reporting;
+using RdlcWebApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RdlcWebApi.Services
+{
+    public interface IReportService
+    {
+        byte[] GenerateReportAsync(string reportName, string reportType);
+    }
+    public class ReportService : IReportService
+    {
+        public byte[] GenerateReportAsync(string reportName, string reportType)
+        {
+
+            string fileDirPath = Assembly.GetExecutingAssembly().Location.Replace("RdlcWebApi.dll",string.Empty);
+            string rdlcFilePath = string.Format("{0}ReportFiles\\{1}.rdlc", fileDirPath, reportName);
+
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            Encoding.GetEncoding("utf-8");
+
+            LocalReport report = new LocalReport(rdlcFilePath);
+
+            //prepare data for report
+
+            List<UserDto> userList = new List<UserDto>();
+
+            var user1 = new UserDto { FirstName = "Matías", LastName = "Ibarra", Email = "matiasibarra96@hotmail.com", Phone = "3547563985" };
+
+            userList.Add(user1);
+
+            report.AddDataSource("dsUsers", userList);
+
+            Dictionary<string, string> parameters = new Dictionary<string, string>();
+            var result = report.Execute(GetRenderType(reportType), 1, parameters);
+
+            return result.MainStream;
+        }
+
+        private RenderType GetRenderType(string reportType)
+        {
+            var renderType = RenderType.Pdf;
+            switch (reportType.ToUpper())
+            {
+                default:
+                case "PDF":
+                    renderType = RenderType.Pdf;
+                    break;
+                case "XLS":
+                    renderType = RenderType.Excel;
+                    break;
+                case "WORD":
+                    renderType = RenderType.Word;
+                    break;
+            }
+
+            return renderType;
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,92 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using RdlcWebApi.Services;
+
+
+namespace RdlcWebApi
+{
+    public class Startup
+    {
+        private string _MyCors;
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy(name: _MyCors, builder =>
+                {
+                    builder.WithOrigins("*").AllowAnyHeader()
+                                                  .AllowAnyMethod(); ;
+                    //builder.SetIsOriginAllowed(origin => new Uri(origin).Host == "*")
+                    //.AllowAnyHeader().AllowAnyMethod();
+                });
+            });
+
+
+
+
+
+
+
+            services.AddSwaggerGen();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Implement Swagger UI",
+                    Description = "A simple example to Implement Swagger UI",
+                });
+            });
+
+            services.AddScoped<IReportService, ReportService>();
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(c => {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Showing API V1");
+            });
+
+
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
